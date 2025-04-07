b6bd9d05cd19dc83425d148e6ad42f2bfb60dd65
YeikooM <yeiki659@gmail.com>
2025-04-07 13:22:24 -0500

    cargar proyecto en repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/contenedor/cabecera/cabecera.component';
+import { InicioComponent } from './components/contenedor/inicio/inicio.component';
+import { ErrorComponent } from './components/contenedor/error/error.component';
+import { ContactoComponent } from './components/contenedor/contacto/contacto.component';
+import { AcercaDeComponent } from './components/contenedor/acerca-de/acerca-de.component';
+import { FooterComponent } from './components/contenedor/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    InicioComponent,
+    ErrorComponent,
+    ContactoComponent,
+    AcercaDeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
