f76f0f99e29592b8a6def06e45b3db3d5ce8d42e
YeikooM <yeiki659@gmail.com>
2025-05-04 14:55:16 -0500

    Tus cambios

--- src/app/modulos/contenedor/dash/cabecera-dash/cabecera-dash.component.ts
+++ src/app/modulos/contenedor/dash/cabecera-dash/cabecera-dash.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
+import { Directive, HostListener, Output, EventEmitter, ElementRef, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-cabecera-dash',
   templateUrl: './cabecera-dash.component.html',
-  styleUrl: './cabecera-dash.component.css'
+  styleUrls: ['./cabecera-dash.component.css']
 })
-export class CabeceraDashComponent {
+export class CabeceraDashComponent implements OnInit {
 
-}
+  ngOnInit(): void {
+    this.initTyped();
+  }
+
+  private initTyped(): void {
+    const options = {
+      strings: ['Engineer', 'Programmer', 'Freelancer', 'Coach'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      startDelay: 500,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+    };
+
+    new Typed('.typed-element', options);
+  }
+  
+    @Output() scrollToTop = new EventEmitter<void>();
+  
+    @HostListener('click') onClick() {
+      this.scrollToTop.emit();
+      window.scroll({ top: 0, behavior: 'smooth' });
+    }
+  
+    constructor(private el: ElementRef, private renderer: Renderer2) {
+      this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+    }
+  }
+
+  
